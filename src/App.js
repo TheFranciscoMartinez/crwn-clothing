8d829be09e07190157e387889b033f4be4c5a7b0
Francisco Martinez <franciscomartinez@Franciscos-MacBook-Pro.local>
2021-09-13 13:19:07 -0700

    broken home page into smaller conponents, finshed styling menu item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
 
 function App() {
   return (
     <div>
      <HomePage />
-     <h1>Testing</h1>
     
     </div>
   );
