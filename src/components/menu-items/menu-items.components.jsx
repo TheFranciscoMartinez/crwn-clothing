78d9afa669c00af519c47e45354594bc59f0f3a4
Francisco Martinez <franciscomartinez@Franciscos-MacBook-Pro.local>
2021-09-14 11:51:27 -0700

    update our menu items to navigate with router HOC

--- src/components/menu-items/menu-items.components.jsx
+++ src/components/menu-items/menu-items.components.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {withRouter} from 'react-router-dom';
 import './menu-item.style.scss'
 
 
-const menuItem = ({title, imageUrl, size}) => (
 
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+
+    <div className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
 
     <div className='background-image'
         style={{
@@ -16,7 +19,7 @@ const menuItem = ({title, imageUrl, size}) => (
             <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
         </div>
-        
+
     </div>
 );
-export default menuItem;
+export default withRouter(MenuItem);
