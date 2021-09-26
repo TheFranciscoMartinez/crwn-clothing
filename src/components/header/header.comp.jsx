f8bf3ecf6009207df460381043d54bef0c3d71c4
Francisco Martinez <franciscomartinez@Franciscos-MacBook-Pro.local>
2021-09-26 15:17:08 -0700

    added redux programing for cart item, included toggle action to cart icon

--- src/components/header/header.comp.jsx
+++ src/components/header/header.comp.jsx
@@ -4,10 +4,14 @@ import './header.style.scss'
 import {ReactComponent as Logo} from '../../../src/assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.comp";
+import CartDropdown from "../cart-dropdown/cart-dropdown.comp";
 
 
 
-const Header = ({currentUser}) => (
+
+
+const Header = ({currentUser, hidden}) => (
 
     <div className='header'>
 
@@ -18,20 +22,25 @@ const Header = ({currentUser}) => (
         <div className="options">
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
-            {
-                  currentUser ? 
-                  <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                  :
-                  <Link className='option' to='/signin'>SIGN IN</Link>
-            }
 
+            {
+                  currentUser ? (
+                  <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                  ) : (
+                  <Link className='option' to='/signin'>
+                  SIGN IN
+                  </Link> 
+                  )}
+
+                <CartIcon />
         </div>
-    
+                {hidden ? null : <CartDropdown />}
     </div>
 );
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) =>({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
